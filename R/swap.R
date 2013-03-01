50f514faab1370086bbb4523a45b074fd5dfc8ee
Kevin Ushey <kevinushey@gmail.com>
2013-02-28 17:19:26 -0800

    don't export swap

--- R/swap.R
+++ R/swap.R
@@ -7,24 +7,7 @@
 #' @param to the items you will be mapping 'to'. must be same length and
 #' order as \code{from}.
 #' @param ... optional arguments passed to \code{match}.
-#' @export
 #' @seealso \code{\link{match}}
-#' @examples
-#' x <- c(1, 2, 2, 3)
-#' from <- c(1, 2)
-#' to <- c(10, 20)
-#' swap( x, from, to )
-#' 
-#' ## alternatively, we can submit a named character vector
-#' ## we translate from value to name. note that this forces
-#' ## a conversion to character
-#' names(from) <- to
-#' swap( x, from )
-#' 
-#' ## NAs are handled sensibly. Types are coerced as needed.
-#' x <- c(1, NA, 2, 2, 3)
-#' swap(x, c(1, 2), c("a", "b") )
-#' 
 swap <- function( vec, from, to=names(from), ... ) {
   tmp <- to[ match( vec, from, ... ) ]
   tmp[ is.na(tmp) ] <- vec[ is.na(tmp) ]
