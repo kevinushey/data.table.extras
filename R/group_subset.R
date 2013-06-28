eaaea3dcd98fea2db1d344b4582835293bf2a14b
Kevin Ushey <kevinushey@gmail.com>
2013-06-27 20:01:27 -0700

    group subsetting: get the nth row of a DT, split by some grouping var

---
+++ R/group_subset.R
@@ -0,0 +1,30 @@
+#' Row Subset by Group
+#' 
+#' For each \code{data.table} subset (as subset by the \code{by} argument),
+#' return the rows at indices \code{i}.
+#' 
+#' The code wraps to the impossible to remember but incredibly powerful and
+#' fast \code{DT[ DT[, .I[i], by=by]$V1 ]}.
+#' 
+#' @param DT A \code{data.table}.
+#' @param i Indices at which to subset each sub-\code{data.table}.
+#' @param by Variable(s) to subset over.
+#' @export
+#' @examples
+#' DT <- data.table(
+#'   x=rep(letters, each=1E5),
+#'   y=rnorm(26*1E5),
+#'   z=rep(LETTERS, each=1E5),
+#'   a=rep(LETTERS, each=1E5),
+#'   b=rep(LETTERS, each=1E5),
+#'   c=rep(LETTERS, each=1E5),
+#'   d=rep(LETTERS, each=1E5),
+#'   e=rep(LETTERS, each=1E5)
+#' )
+#' 
+#' DT[, mean_y := mean(y), by=x]
+#' group_subset(DT, 1, x)
+group_subset <- function(DT, i=1, by) {
+  call <- match.call()
+  return( DT[ DT[, .I[i], by=eval( call$by )]$V1 ] )
+}
